add512eaf29f3f0c881c6fe0bda911079d9f7cdb
miskhill <80961839+miskhill@users.noreply.github.com>
2022-03-13 13:59:25 +0000

    Adding more links

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,7 +1,16 @@
-//import react from "react"
+import Link from 'next/link'
 
 function FirstPost() {
-    return <h1>hubby hubby </h1>
-  }
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  )
+}
 
-  export default FirstPost
+export default FirstPost
