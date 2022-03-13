b3fc2236b8720ef7210806eca60c700190da647d
miskhill <80961839+miskhill@users.noreply.github.com>
2022-03-13 14:28:10 +0000

    CSS files and directory added

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,10 +1,11 @@
-import Link from 'next/link'
 import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../../components/layout'
 
 function FirstPost() {
   return (
-    <>
-     <Head>
+    <Layout>
+      <Head>
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>
@@ -13,7 +14,7 @@ function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   )
 }
 
