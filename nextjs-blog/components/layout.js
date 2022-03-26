d6549bed5d849da66a4ccdacab560df9efa8f04e
miskhill <80961839+miskhill@users.noreply.github.com>
2022-03-26 15:01:16 +0000

    Variable

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -1,5 +1,7 @@
 import styles from '../components/layout.module.css'
 
+const name = '[Gary Smith]'
+
 function Layout({ children }) {
   return <div className={styles.container}>{children}</div>
 }
